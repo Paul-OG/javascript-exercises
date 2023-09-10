dcad9a75011f5dd267c03cef2a656b7a23082f3f
Paul-OG <58085399+Paul-OG@users.noreply.github.com>
2023-09-10 21:39:20 +0100

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
 
+    if (number < 0) {
+        return 'ERROR';
+    }
+    
+    let bigString = '';
+    for(i = 0; i < number; i++) {
+        bigString += string;
+    }
+    return bigString
 };
 
 // Do not edit below this line
