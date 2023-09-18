4595ea0a41899ed2a359719f730efda59ccaba47
Paul-OG <58085399+Paul-OG@users.noreply.github.com>
2023-09-18 13:45:13 +0100

    completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  //function to convert fahrenheit to celsius
+  //formula: (fahrenheit - 32) * 5/9
+  //return the result
+  return Math.round(((tempFahrenheit - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  //function to convert celsius to fahrenheit
+  //formula: (celsius * 9/5) + 32
+  //round to 1 decimal
+  //return the result
+  return Math.round(((tempCelsius * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
